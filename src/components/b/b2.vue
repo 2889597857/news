37bdf43f757b1033491f3ae70f8a8fb31ee54b26
whc <49740897+2889597857@users.noreply.github.com>
2021-11-17 12:24:54 +0800

    123

---
+++ src/components/b/b2.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>b2</h1>
+</template>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,4 +1,9 @@
+<template>
+    <h1>Layout</h1>
+    <main />
+</template>
 <script lang="ts"  setup>
+import main from './components/main.vue';
 </script>
 <style scoped lang="scss">
 </style>

ca02baafba726c0587f792c231b3bf5c2357b876
whc <49740897+2889597857@users.noreply.github.com>
2021-11-18 16:34:09 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
 <template lang="pug">
 router-view
 </template>
-<script lang="ts" setup>
-import { emitter } from '@/utils/mitt';
-import { getCurrentInstance } from 'vue-demi';
-emitter.on('foo', e => console.log('foo', e))
-// console.log(getCurrentInstance().appContext.config.globalProperties.$storage)
-</script>

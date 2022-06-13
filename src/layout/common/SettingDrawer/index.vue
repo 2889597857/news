a908cb8770ed8435806ab46853702bb1f3780190
whc <2889597857@qq.com>
2022-06-13 17:18:58 +0800

    autocom

--- src/layout/common/SettingDrawer/index.vue
+++ src/layout/common/SettingDrawer/index.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>Setting</h1>
+    <n-drawer>
+        <n-drawer-content>
+            <h1>Setting</h1>
+        </n-drawer-content>
+    </n-drawer>
+    <b></b>
 </template>
+<script lang="ts" setup>
+
+const seting = () => {
+
+}
+</script>
+
+<style scoped lang="scss">
+</style>

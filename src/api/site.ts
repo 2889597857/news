a626c26e4bf3beafa384b0501be82201debc4ae9
zhandehuang <2889597857@qq.com>
2023-03-01 17:30:06 +0800

    新api

--- src/api/site.ts
+++ src/api/site.ts
@@ -3,3 +3,6 @@ import { axios } from './index';
 export async function getWebsite() {
   return axios.get<Array<Site.Info>>(`/site/info`);
 }
+export async function searchSiteName(name) {
+  return axios.get<Array<Site.Info>>(`/site/find/name?name=${name}`);
+}
